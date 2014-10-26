e828245e06729ecbd87e3d1ad7bd307bcd02dd32
Trey Dockendorf <treydock@gmail.com>
2014-10-26 15:28:56 -0500

    Update using modulesync

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,30 +1,11 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
-require 'lib/module_spec_helper'
-
-begin
-  require 'simplecov'
-  require 'coveralls'
-  SimpleCov.formatter = Coveralls::SimpleCov::Formatter
-  SimpleCov.start do
-    add_filter '/spec/'
-  end
-rescue Exception => e
-  warn "Coveralls disabled"
-end
 
 dir = File.expand_path(File.dirname(__FILE__))
-Dir["#{dir}/support/**/*.rb"].sort.each {|f| require f}
+Dir["#{dir}/support/**/*.rb"].sort.each { |f| require f }
 
-shared_context :defaults do
-  let :default_facts do
-    {
-      :fqdn                       => 'foo.example.com',
-      :domain                     => 'example.com',
-      :osfamily                   => 'RedHat',
-      :root_home                  => '/root',
-      :operatingsystemmajrelease  => '6',
-    }
-  end
+# Workaround for no method in rspec-puppet to pass undef through :params
+class Undef
+  def inspect; 'undef'; end
 end
 
 at_exit { RSpec::Puppet::Coverage.report! }
